0fcc17b13c6262bad2d2563185bf38c3db58fb05
Neuromancer93 <itrejonumber1@gmail.com>
2019-11-19 23:56:25 -0800

    feat: added circle avatar to search page

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -13,7 +13,14 @@ class _SearchScreenState extends State<SearchScreen> {
         child: Column(
           children: <Widget>[
             //Todo: Create better UI for the search feature. Must access database correctly.
-            Text('Search for a User/Post')
+            Text('Search for a User/Post'),
+            Container(
+              child: CircleAvatar(
+                radius: 50.0,
+                backgroundImage:
+                    NetworkImage('https://i.redd.it/dmdqlcdpjlwz.jpg'),
+              ),
+            )
           ],
         ),
       ),
